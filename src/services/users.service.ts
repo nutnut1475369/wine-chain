d29452a8faf38c0db027cb5f60b1c31fc1161751
nut <nutnut1475369@hotmail.com>
2021-07-14 00:49:05 +0700

    Add status return

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '@entities/users.entity';
@@ -9,7 +9,6 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-
   private readonly logger = new Logger(UsersService.name);
 
   constructor(
@@ -21,30 +20,50 @@ export class UsersService {
     private AddressesRepository: Repository<Address>,
   ) {}
 
-  async create(createUserDto:CreateUserDto): Promise<User>{
+  async create(createUserDto: CreateUserDto): Promise<User> {
     const producer = new Producer();
-        producer.producerSymbol = createUserDto.userProducerSymbol
-        producer.producerSerialCount = 0
-    const producerid = await this.ProducersRepository.save(Producer)
+    producer.producerSymbol = createUserDto.userProducerSymbol;
+    producer.producerSerialCount = 0;
+    const producerid = await this.ProducersRepository.save(Producer);
     const address = new Address();
-        address.addressAddress = createUserDto.userAddressAddress
-        address.addressStreetName = createUserDto.userAddressStreetName
-        address.addressState = createUserDto.userAddressState
-        address.addressPostcode = createUserDto.userAddressPostcode
-        address.addressCity = createUserDto.userAddressCity
-        address.addressCountry = createUserDto.userAddressCountry
-    const addressid = await this.AddressesRepository.save(address)
+    address.addressAddress = createUserDto.userAddressAddress;
+    address.addressStreetName = createUserDto.userAddressStreetName;
+    address.addressState = createUserDto.userAddressState;
+    address.addressPostcode = createUserDto.userAddressPostcode;
+    address.addressCity = createUserDto.userAddressCity;
+    address.addressCountry = createUserDto.userAddressCountry;
+    const addressid = await this.AddressesRepository.save(address);
     const user = new User();
-        user.userName = createUserDto.userName
-        user.userLastname = createUserDto.userLastname
-        user.userEmail = createUserDto.userEmail
-        user.userProducerId = producerid.producerId
-        user.userRoleId = createUserDto.userRoleId
-        user.userTypeId = createUserDto.userTypeId
-        user.userAddressId = addressid.addressId
-        user.userPassword = await bcrypt.hash(createUserDto.userPassword,12)
-    this.logger.log(user)
-    return ;
+    user.userName = createUserDto.userName;
+    user.userLastname = createUserDto.userLastname;
+    user.userEmail = createUserDto.userEmail;
+    user.userProducerId = producerid.producerId;
+    user.userRoleId = createUserDto.userRoleId;
+    user.userTypeId = createUserDto.userTypeId;
+    user.userAddressId = addressid.addressId;
+    user.userPassword = await bcrypt.hash(createUserDto.userPassword, 12);
+    this.logger.log(user);
+    try {
+      return await this.usersRepository.save(User);
+    } catch (error) {
+      if (error) {
+        throw new HttpException(
+          {
+            status: HttpStatus.BAD_REQUEST,
+            error: 'Have something wrong',
+          },
+          HttpStatus.BAD_REQUEST,
+        );
+      } else {
+        throw new HttpException(
+          {
+            status: HttpStatus.OK,
+            error: 'Success',
+          },
+          HttpStatus.OK,
+        );
+      }
+    }
   }
 
   findAll(): Promise<User[]> {
